59cad8f143a2c8dae58f845a082b6a0a70405f56
kqns91 <kansai.119917@gmail.com>
2024-07-08 23:02:57 +0900

    update: stdin

--- creating_programming_languages/rustack/src/main.rs
+++ creating_programming_languages/rustack/src/main.rs
@@ -1,20 +1,27 @@
 fn main() {
-    let mut stack = vec![];
+    for line in std::io::stdin().lines() {
+        let mut stack = vec![];
+        if let Ok(line) = line {
+            let words: Vec<_> = line.split(" ").collect();
 
-    stack.push(42);
-    stack.push(36);
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
 
-    add(&mut stack);
-
-    stack.push(22);
-
-    add(&mut stack);
-
-    println!("stack: {stack:?}");
+            println!("stack: {stack:?}");
+        }
+    }
 }
 
 fn add(stack: &mut Vec<i32>) {
-    let lhs = stack.pop().unwrap();
-    let rhs = stack.pop().unwrap();
-    stack.push(lhs + rhs);
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    stack.push(a + b);
 }
