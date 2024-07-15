0cf9062883028263b469c06fd78d6f3375fc7332
kqns91 <kansai.119917@gmail.com>
2024-07-16 00:13:45 +0900

    add: if

--- creating_programming_languages/rustack/src/main.rs
+++ creating_programming_languages/rustack/src/main.rs
@@ -17,17 +17,15 @@ fn parse<'a>(line: &'a str) -> Vec<Value> {
             let value;
             (value, rest) = parse_block(rest);
             stack.push(value);
-        } else if let Ok(parsed) = word.parse::<i32>() {
-            stack.push(Value::Num(parsed));
         } else {
-            match word {
-                "+" => add(&mut stack),
-                "-" => sub(&mut stack),
-                "*" => mul(&mut stack),
-                "/" => div(&mut stack),
-                _ => panic!("{word:?} could not be parsed"),
-            }
+            let code = if let Ok(num) = word.parse::<i32>() {
+                Value::Num(num)
+            } else {
+                Value::Op(word)
+            };
+            eval(code, &mut stack);
         }
+
         words = rest;
     }
 
@@ -61,11 +59,25 @@ fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str
     (Value::Block(tokens), words)
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Value<'src> {
+    Num(i32),
+    Op(&'src str),
+    Block(Vec<Value<'src>>),
+}
+
 impl<'src> Value<'src> {
     fn as_num(&self) -> i32 {
         match self {
             Self::Num(val) => *val,
             _ => panic!("Value is not a number"),
+        }
+    }
+
+    fn to_block(self) -> Vec<Value<'src>> {
+        match self {
+            Self::Block(val) => val,
+            _ => panic!("Value is not a block"),
         }
     }
 }
@@ -94,13 +106,6 @@ fn div(stack: &mut Vec<Value>) {
     stack.push(Value::Num(b / a));
 }
 
-#[derive(Debug, PartialEq, Eq)]
-enum Value<'src> {
-    Num(i32),
-    Op(&'src str),
-    Block(Vec<Value<'src>>),
-}
-
 #[cfg(test)]
 mod test {
     use super::{parse, Value::*};
@@ -110,5 +115,51 @@ mod test {
             parse("1 2 + { 3 4 }"),
             vec![Num(3), Block(vec![Num(3), Num(4)])]
         );
+    }
+
+    #[test]
+    fn test_if_false() {
+        assert_eq!(parse("{ 1 -1 + } { 100 } { -100 } if"), vec![Num(-100)]);
+    }
+
+    #[test]
+    fn test_if_true() {
+        assert_eq!(parse("{ 1 1 + } { 100 } { -100 } if"), vec![Num(100)]);
+    }
+}
+
+fn eval<'src>(code: Value<'src>, stack: &mut Vec<Value<'src>>) {
+    match code {
+        Value::Op(op) => match op {
+            "+" => add(stack),
+            "-" => sub(stack),
+            "*" => mul(stack),
+            "/" => div(stack),
+            "if" => op_if(stack),
+            _ => panic!("{op:?} could not be parsed"),
+        },
+        _ => stack.push(code.clone()),
+    }
+}
+
+fn op_if(stack: &mut Vec<Value>) {
+    let false_branch = stack.pop().unwrap().to_block();
+    let true_branch = stack.pop().unwrap().to_block();
+    let cond = stack.pop().unwrap().to_block();
+
+    for code in cond {
+        eval(code, stack);
+    }
+
+    let cond_result = stack.pop().unwrap().as_num();
+
+    if cond_result != 0 {
+        for code in true_branch {
+            eval(code, stack);
+        }
+    } else {
+        for code in false_branch {
+            eval(code, stack);
+        }
     }
 }
