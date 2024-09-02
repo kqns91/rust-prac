4890f2f80dba6bf6cfa23408df8d89769c86633a
kqns91 <kansai.119917@gmail.com>
2024-09-02 22:14:38 +0900

    add: 2.9

--- creating_programming_languages/rustack/src/main.rs
+++ creating_programming_languages/rustack/src/main.rs
@@ -3,6 +3,110 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Value {
+    Num(i32), // 数値
+    Op(String), // 演算子
+    Sym(String), // 変数名を示すためのシンボル
+    Block(Vec<Value>), // ブロック
+    Native(NativeOp), // 組み込み演算子
+}
+
+impl Value {
+    fn as_num(&self) -> i32 {
+        match self {
+            Self::Num(val) => *val,
+            _ => panic!("Value is not a number"),
+        }
+    }
+
+    fn to_block(self) -> Vec<Value> {
+        match self {
+            Self::Block(val) => val,
+            _ => panic!("Value is not a block"),
+        }
+    }
+
+    fn as_sym(&self) -> &str {
+        if let Self::Sym(sym) = self {
+            sym
+        } else {
+            panic!("Value is not a symbol");
+        }
+    }
+
+    fn to_string(&self) -> String {
+        match self {
+            Self::Num(i) => i.to_string(),
+            Self::Op(ref s) | Self::Sym(ref s) => s.clone(),
+            Self::Block(_) => "<Block>".to_string(),
+            Self::Native(_) => "<Native>".to_string(),
+        }
+    }
+}
+
+// タプル構造体：指定した要素で構成されたタプル。golang の型埋め込み的な感じ。
+// 関数ポインタを中身にもつタプル構造体。newtype パターンと呼ばれる。
+// Rust では関数ポインタに対する Debug, PartialEq, Eq トレイトを derive 属性で自動的に定義できない。
+// 外部クレートや標準ライブラリの型にトレイトを実装できない。
+// 組み込みの演算子。
+#[derive(Clone)]
+struct NativeOp(fn(&mut Vm));
+
+// 基本 Eq と両方実装する。
+impl PartialEq for NativeOp {
+    fn eq(&self, other: &NativeOp) -> bool {
+        self.0 as *const fn() == other.0 as *const fn()
+    }
+}
+
+// メソッドのないトレイト。
+impl Eq for NativeOp {}
+
+impl std::fmt::Debug for NativeOp {
+    fn fmt(
+        &self, 
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        write!(f, "<NativeOp>")
+    }
+}
+
+#[derive(Debug)]
+struct Vm {
+    stack: Vec<Value>,
+    vars: HashMap<String, Value>,
+    blocks: Vec<Vec<Value>>,
+}
+
+impl Vm {
+    fn new() -> Self {
+        let functions: [(&str, fn(&mut Vm)); 12] = [
+            ("+", add),
+            ("-", sub),
+            ("*", mul),
+            ("/", div),
+            ("<", lt),
+            ("if", op_if),
+            ("def", op_def),
+            ("puts", puts),
+            ("pop", pop),
+            ("dup", dup),
+            ("exch", exch),
+            ("index", index),
+        ];
+        Self {
+            stack: vec![],
+            vars: functions
+            .into_iter()
+            .map(|(name, fun)| {
+                (name.to_owned(), Value::Native(NativeOp(fun)))
+            }).collect(),
+            blocks: vec![],
+        }
+    }
+}
+
 fn main() {
     if let Some(f) = std::env::args()
         .nth(1)
@@ -55,45 +159,105 @@ fn parse_word(word: &str, vm: &mut Vm) {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum Value {
-    Num(i32),
-    Op(String),
-    Sym(String), // 変数名を示すためのシンボル
-    Block(Vec<Value>),
-    // Native(NativeOp),
+fn eval(code: Value, vm: &mut Vm) {
+    if let Some(top_block) = vm.blocks.last_mut() {
+        top_block.push(code);
+        return;
+    }
+    if let Value::Op(ref op) = code {
+        let val: Value = vm
+            .vars
+            .get(op)
+            .expect(&format!("{op:?} is not a defined operation"))
+            .clone();
+        match  val {
+            Value::Block(block) => {
+                for code in block {
+                    eval(code, vm);
+                }
+            }
+            Value::Native(op) => op.0(vm),
+            _ => vm.stack.push(val),
+        } 
+    } else {
+        vm.stack.push(code.clone());
+    }
 }
 
-impl Value {
-    fn as_num(&self) -> i32 {
-        match self {
-            Self::Num(val) => *val,
-            _ => panic!("Value is not a number"),
+macro_rules! impl_op {
+    {$name:ident, $op:tt} => {
+        fn $name(vm: &mut Vm) {
+            let rhs = vm.stack.pop().unwrap().as_num();
+            let lhs = vm.stack.pop().unwrap().as_num();
+            vm.stack.push(Value::Num((lhs $op rhs) as i32));
         }
+    };
+}
+
+impl_op!(add, +);
+impl_op!(sub, -);
+impl_op!(mul, *);
+impl_op!(div, /);
+impl_op!(lt, <);
+
+fn op_if(vm: &mut Vm) {
+    let false_branch = vm.stack.pop().unwrap().to_block();
+    let true_branch = vm.stack.pop().unwrap().to_block();
+    let cond = vm.stack.pop().unwrap().to_block();
+
+    for code in cond {
+        eval(code, vm);
     }
 
-    fn to_block(self) -> Vec<Value> {
-        match self {
-            Self::Block(val) => val,
-            _ => panic!("Value is not a block"),
-        }
-    }
+    let cond_result = vm.stack.pop().unwrap().as_num();
 
-    fn as_sym(&self) -> &str {
-        if let Self::Sym(sym) = self {
-            sym
-        } else {
-            panic!("Value is not a symbol");
+    if cond_result != 0 {
+        for code in true_branch {
+            eval(code, vm);
+        }
+    } else {
+        for code in false_branch {
+            eval(code, vm);
         }
     }
+}
 
-    fn to_string(&self) -> String {
-        match self {
-            Self::Num(i) => i.to_string(),
-            Self::Op(ref s) | Self::Sym(ref s) => s.clone(),
-            Self::Block(_) => "<Block>".to_string(),
-        }
-    }
+fn op_def(vm: &mut Vm) {
+    let value = vm.stack.pop().unwrap();
+    eval(value, vm);
+    let value = vm.stack.pop().unwrap();
+    let sym = vm.stack.pop().unwrap().as_sym().to_string();
+
+    vm.vars.insert(sym, value);
+}
+
+fn puts(vm: &mut Vm) {
+    let value = vm.stack.pop().unwrap();
+    println!("{}", value.to_string());
+}
+
+fn pop(vm: &mut Vm) {
+    vm.stack.pop().unwrap();
+}
+
+fn index(vm: &mut Vm) {
+let index = vm.stack.pop().unwrap().as_num() as usize;
+let value = vm.stack[vm.stack.len() - index - 1].clone();
+vm.stack.push(value);
+}
+
+// duplicate. スタックの最上にある値を複製しプッシュする
+fn dup(vm: &mut Vm) {
+    let value = vm.stack.last().unwrap();
+    vm.stack.push(value.clone());
+}
+
+// exchange. スタックの最上位の2つの値を交換する
+fn exch(vm: &mut Vm) {
+    let last = vm.stack.pop().unwrap();
+    let second = vm.stack.pop().unwrap();
+    vm.stack.push(last);
+    vm.stack.push(second);
 }
 
 #[cfg(test)]
@@ -154,99 +318,3 @@ if
         );
     }
 }
-
-fn eval(code: Value, vm: &mut Vm) {
-    if let Some(top_block) = vm.blocks.last_mut() {
-        top_block.push(code);
-        return;
-    }
-    match code {
-        Value::Op(ref op) => match op as &str {
-            "+" => add(&mut vm.stack),
-            "-" => sub(&mut vm.stack),
-            "*" => mul(&mut vm.stack),
-            "/" => div(&mut vm.stack),
-            "<" => lt(&mut vm.stack),
-            "if" => op_if(vm),
-            "def" => op_def(vm),
-            "puts" => puts(vm),
-            _ => {
-                let val = vm
-                    .vars
-                    .get(op)
-                    .expect(&format!("{op:?} is not a defined operation"));
-                vm.stack.push(val.clone());
-            }
-        },
-        _ => vm.stack.push(code.clone()),
-    }
-}
-
-fn op_if(vm: &mut Vm) {
-    let false_branch = vm.stack.pop().unwrap().to_block();
-    let true_branch = vm.stack.pop().unwrap().to_block();
-    let cond = vm.stack.pop().unwrap().to_block();
-
-    for code in cond {
-        eval(code, vm);
-    }
-
-    let cond_result = vm.stack.pop().unwrap().as_num();
-
-    if cond_result != 0 {
-        for code in true_branch {
-            eval(code, vm);
-        }
-    } else {
-        for code in false_branch {
-            eval(code, vm);
-        }
-    }
-}
-
-fn op_def(vm: &mut Vm) {
-    let value = vm.stack.pop().unwrap();
-    eval(value, vm);
-    let value = vm.stack.pop().unwrap();
-    let sym = vm.stack.pop().unwrap().as_sym().to_string();
-
-    vm.vars.insert(sym, value);
-}
-
-fn puts(vm: &mut Vm) {
-    let value = vm.stack.pop().unwrap();
-    println!("{}", value.to_string());
-}
-
-#[derive(Debug)]
-struct Vm {
-    stack: Vec<Value>,
-    vars: HashMap<String, Value>,
-    blocks: Vec<Vec<Value>>,
-}
-
-impl Vm {
-    fn new() -> Self {
-        Self {
-            stack: vec![],
-            vars: HashMap::new(),
-            blocks: vec![],
-        }
-    }
-}
-
-macro_rules! impl_op {
-    {$name:ident, $op:tt} => {
-        fn $name(stack: &mut Vec<Value>) {
-            let rhs = stack.pop().unwrap().as_num();
-            let lhs = stack.pop().unwrap().as_num();
-            stack.push(Value::Num((lhs $op rhs) as i32));
-        }
-    };
-}
-
-impl_op!(add, +);
-impl_op!(sub, -);
-impl_op!(mul, *);
-impl_op!(div, /);
-impl_op!(lt, <);
